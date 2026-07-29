85861d8e0e1a1e0280dd291bc3a10501dac46100
Yanabeaalhuda <mw@hudahotels.com>
2026-07-29 19:15:31 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='yanabea-individual-quote-v13';
+const CACHE_NAME='yanabea-individual-quote-v14';
 const APP_FILES=['./','./index.html','./404.html','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./logo.png'];
 
 self.addEventListener('install',event=>{
